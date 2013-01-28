9f0f888b15bbf5c95ddd20907eab4f54dc185adf
Yang Tao <thomas.tao.yang@gmail.com>
2013-01-28 15:52:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the sample application for
+[*Ruby on Rails Turorial: Learn Rails by Example*](http://railstutorial.org)
+by [Michael Hart](http://michaelhartl.com/)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2c85933fc64707c48e1e6fbfcd88a8be7d0047107cc4cde231a31cbc9406720688c95a946fa1f9f76e235f659a3e37678ec3009f0dc2f6a754e4d99ac82c24f0'
